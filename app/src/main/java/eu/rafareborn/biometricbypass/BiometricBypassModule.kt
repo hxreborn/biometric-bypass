eebfac6165ad1edfef131b551ecc60301316694c
Rafa Reborn <hxreborn@duck.com>
2026-03-17 19:45:20 +0100

    feat!: migrate hooks to API 101 interceptor chain
    Signed-off-by: Rafa Reborn <hxreborn@duck.com>

--- app/src/main/java/eu/rafareborn/biometricbypass/BiometricBypassModule.kt
+++ app/src/main/java/eu/rafareborn/biometricbypass/BiometricBypassModule.kt
@@ -1,50 +1,32 @@
 package eu.rafareborn.biometricbypass
 
 import android.annotation.SuppressLint
+import android.util.Log
 import eu.rafareborn.biometricbypass.hooker.BiometricBypassHooker
-import io.github.libxposed.api.XposedInterface
 import io.github.libxposed.api.XposedModule
 import io.github.libxposed.api.XposedModuleInterface.ModuleLoadedParam
-import io.github.libxposed.api.XposedModuleInterface.PackageLoadedParam
+import io.github.libxposed.api.XposedModuleInterface.PackageReadyParam
 
-internal lateinit var module: BiometricBypassModule
-
-class BiometricBypassModule(
-    base: XposedInterface,
-    param: ModuleLoadedParam,
-) : XposedModule(base, param) {
-    init {
-        module = this
+class BiometricBypassModule : XposedModule() {
+    override fun onModuleLoaded(param: ModuleLoadedParam) {
+        log(Log.INFO, TAG, "BiometricBypass v${BuildConfig.VERSION_NAME} loaded")
     }
 
     @SuppressLint("PrivateApi")
-    override fun onPackageLoaded(param: PackageLoadedParam) {
+    override fun onPackageReady(param: PackageReadyParam) {
         if (param.packageName != TARGET_PACKAGE || !param.isFirstPackage) return
 
-        module.log("$TAG Loaded package: ${param.packageName}")
-
         try {
-            hookTargetMethod(param.classLoader)
+            BiometricBypassHooker.hook(this, param.classLoader)
         } catch (e: ReflectiveOperationException) {
-            module.log("$TAG Error: ${e::class.simpleName} - ${e.message}")
+            log(Log.ERROR, TAG, "Error: ${e::class.simpleName} - ${e.message}")
         } catch (e: Exception) {
-            module.log("$TAG Unexpected error: ${e.message}")
+            log(Log.ERROR, TAG, "Unexpected error: ${e.message}")
         }
     }
 
-    @SuppressLint("PrivateApi")
-    private fun hookTargetMethod(classLoader: ClassLoader) {
-        val targetClass = classLoader.loadClass(TARGET_CLASS)
-        val targetMethod = targetClass.getDeclaredMethod(TARGET_METHOD)
-        module.hook(targetMethod, BiometricBypassHooker::class.java)
-        module.log("$TAG Successfully hooked method: $TARGET_METHOD in $TARGET_CLASS")
-    }
-
     companion object {
-        const val TAG = "BiometricBypassModule"
+        const val TAG = "BiometricBypass"
         private const val TARGET_PACKAGE = "com.android.systemui"
-        private const val TARGET_CLASS = "com.android.systemui.biometrics.AuthContainerView"
-        private const val TARGET_METHOD = "onDialogAnimatedIn"
-        const val BUTTON_CONFIRM_ID = "button_confirm"
     }
 }
